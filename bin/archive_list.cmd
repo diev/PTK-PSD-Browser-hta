5bfee9d6a9aa2698fb34bdb5e67169882073fd6d
mozers <mozers@gmail.com>
2011-12-10 11:08:29 +0000

    3.9.4: ??? ????? ????????? ????????? ??????? ???? ?????? ? ????????? ?????? ????? ???????????: 1. ? ??????? ???????? ????????? ????? archive_list.cmd (GetArcFileList) 2. ???? ? ?????? ?????? ??? ?????????? ?????????? ??????, ?? ????? ?????? ????? ??????? ?? ?????????? ?????? (???????????? ? Answer322P)

---
+++ bin/archive_list.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+IF "%1"=="" EXIT 1
+
+SET unpacker=%~dp0\7z.exe
+SET mtmp=%TEMP%\$mtmp$
+IF EXIST %mtmp% (DEL /Q /F %mtmp%\*.*) ELSE (MD %mtmp%)
+PUSHD %mtmp%
+
+:list
+FOR /F "tokens=1,2 skip=5" %%a IN ('%unpacker% e -y %1') DO (
+	if "%%a"=="Extracting" (
+		ECHO %%b
+		CALL :list %%b
+	)
+)
