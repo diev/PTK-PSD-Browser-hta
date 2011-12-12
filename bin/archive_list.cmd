bf759929aba6ca92570264f5eab3fb84cb4aad9e
mozers <mozers@gmail.com>
2011-12-12 15:19:11 +0000

    3.10.3: ????????? ????????????? ???? ????? ????????? ?? (356?). ???????? ?????????? ????????? ??? ?? ??????????????. archive_list.cmd ????????? ?? ????? ????? ??????.

--- bin/archive_list.cmd
+++ bin/archive_list.cmd
@@ -5,11 +5,15 @@ SET unpacker=%~dp0\7z.exe
 SET mtmp=%TEMP%\$mtmp$
 IF EXIST %mtmp% (DEL /Q /F %mtmp%\*.*) ELSE (MD %mtmp%)
 PUSHD %mtmp%
+CALL :list %1
+POPD
+DEL /Q /F %mtmp%\*.*
+EXIT
 
 :list
-FOR /F "tokens=1,2 skip=5" %%a IN ('%unpacker% e -y %1') DO (
-	if "%%a"=="Extracting" (
-		ECHO %%b
-		CALL :list %%b
+FOR /F "tokens=1,2 skip=5" %%A IN ('%unpacker% e -y %1') DO (
+	if "%%A"=="Extracting" (
+		ECHO %%B
+		CALL :list %%B
 	)
 )
